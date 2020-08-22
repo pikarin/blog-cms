4f9aa094027032cccd107f6e04e01fedac72238b
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2020-08-22 15:55:18 +0700

    compile frontend assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .copy('resources/assets/images', 'public/images');
+    .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
